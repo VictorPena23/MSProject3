e4f6d1cf0cd5b5a460ed0111c389723416c8a18a
VictorPena23 <halo_rider200219@yahoo.com>
2023-06-28 01:07:20 -0700

    adding fix to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-//Pages and components
-import Home from './pages/Home';
+//pages and components
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
        <div className='pages'>
         <Routes>
           <Route
-            path="/"
-            element={Home}
+          path='/'
+          element={<Home />}
           />
         </Routes>
        </div>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
