c6fa5930f54da072f52bb3cfdd162de785318779
VictorPena23 <halo_rider200219@yahoo.com>
2023-06-28 21:24:05 -0700

    adding navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages and components
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
